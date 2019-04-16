471595992fecc5cc550679443ab92953efbbe860
Sannikov Kirill <kir.sannikow@mail.ru>
2019-04-16 20:04:33 +0300

    install auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::group(['namespace' => 'Task'], function () {
+    // маршрут для ресурсного контроллера TaskController
+    Route::resource('tasks', 'TaskController');
+});
